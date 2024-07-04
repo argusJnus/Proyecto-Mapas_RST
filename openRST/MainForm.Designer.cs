319e2b8e66a7f208a9e938f8be4fd1430a8e3549
Santiago Tovar Lizárraga <155687388+argusJnus@users.noreply.github.com>
2024-07-03 17:22:03 -0700

    Add files via upload

---
+++ openRST/MainForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Santiago
+ * Date: 13/05/2024
+ * Time: 02:06 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace openRST
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.mapa = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.txtResumen = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.lblArray = new System.Windows.Forms.TextBox();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.btnCalcular = new System.Windows.Forms.Button();
+			this.txtRadio = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.X = new System.Windows.Forms.Label();
+			this.txtY = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtX = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblRows = new System.Windows.Forms.Label();
+			this.lblCols = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.lblName = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.mapa)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(384, 25);
+			this.btnOpen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(115, 57);
+			this.btnOpen.TabIndex = 0;
+			this.btnOpen.Text = "Abrir Mapa";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// mapa
+			// 
+			this.mapa.Location = new System.Drawing.Point(317, 96);
+			this.mapa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.mapa.Name = "mapa";
+			this.mapa.Size = new System.Drawing.Size(971, 513);
+			this.mapa.TabIndex = 1;
+			this.mapa.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.txtResumen);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Controls.Add(this.lblArray);
+			this.panel1.Controls.Add(this.lblResultado);
+			this.panel1.Controls.Add(this.btnCalcular);
+			this.panel1.Controls.Add(this.txtRadio);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.X);
+			this.panel1.Controls.Add(this.txtY);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.txtX);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Enabled = false;
+			this.panel1.Location = new System.Drawing.Point(12, 96);
+			this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(285, 595);
+			this.panel1.TabIndex = 2;
+			// 
+			// txtResumen
+			// 
+			this.txtResumen.Location = new System.Drawing.Point(3, 459);
+			this.txtResumen.Multiline = true;
+			this.txtResumen.Name = "txtResumen";
+			this.txtResumen.ReadOnly = true;
+			this.txtResumen.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtResumen.Size = new System.Drawing.Size(279, 120);
+			this.txtResumen.TabIndex = 14;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(13, 423);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(154, 17);
+			this.label5.TabIndex = 13;
+			this.label5.Text = "Resumen radiométrico:";
+			// 
+			// lblArray
+			// 
+			this.lblArray.Location = new System.Drawing.Point(13, 242);
+			this.lblArray.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.lblArray.Multiline = true;
+			this.lblArray.Name = "lblArray";
+			this.lblArray.ReadOnly = true;
+			this.lblArray.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.lblArray.Size = new System.Drawing.Size(249, 157);
+			this.lblArray.TabIndex = 12;
+			this.lblArray.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.AutoSize = true;
+			this.lblResultado.Location = new System.Drawing.Point(91, 217);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(62, 17);
+			this.lblResultado.TabIndex = 9;
+			this.lblResultado.Text = "Vecinos:";
+			// 
+			// btnCalcular
+			// 
+			this.btnCalcular.Location = new System.Drawing.Point(91, 176);
+			this.btnCalcular.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnCalcular.Name = "btnCalcular";
+			this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+			this.btnCalcular.TabIndex = 8;
+			this.btnCalcular.Text = "Calcular";
+			this.btnCalcular.UseVisualStyleBackColor = true;
+			this.btnCalcular.Click += new System.EventHandler(this.BtnCalcularClick);
+			// 
+			// txtRadio
+			// 
+			this.txtRadio.Location = new System.Drawing.Point(75, 128);
+			this.txtRadio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.txtRadio.Name = "txtRadio";
+			this.txtRadio.Size = new System.Drawing.Size(139, 22);
+			this.txtRadio.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(13, 130);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(55, 23);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Radio";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(155, 81);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(33, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Y";
+			// 
+			// X
+			// 
+			this.X.Location = new System.Drawing.Point(75, 81);
+			this.X.Name = "X";
+			this.X.Size = new System.Drawing.Size(35, 23);
+			this.X.TabIndex = 4;
+			this.X.Text = "X";
+			// 
+			// txtY
+			// 
+			this.txtY.Location = new System.Drawing.Point(155, 57);
+			this.txtY.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.txtY.Name = "txtY";
+			this.txtY.Size = new System.Drawing.Size(59, 22);
+			this.txtY.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 55);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(55, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Celda";
+			// 
+			// txtX
+			// 
+			this.txtX.Location = new System.Drawing.Point(75, 57);
+			this.txtX.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.txtX.Name = "txtX";
+			this.txtX.Size = new System.Drawing.Size(59, 22);
+			this.txtX.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(75, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(123, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Calcular vecinos";
+			// 
+			// lblRows
+			// 
+			this.lblRows.AutoSize = true;
+			this.lblRows.Location = new System.Drawing.Point(25, 36);
+			this.lblRows.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblRows.Name = "lblRows";
+			this.lblRows.Size = new System.Drawing.Size(45, 17);
+			this.lblRows.TabIndex = 3;
+			this.lblRows.Text = "Filas: ";
+			// 
+			// lblCols
+			// 
+			this.lblCols.AutoSize = true;
+			this.lblCols.Location = new System.Drawing.Point(25, 62);
+			this.lblCols.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblCols.Name = "lblCols";
+			this.lblCols.Size = new System.Drawing.Size(74, 17);
+			this.lblCols.TabIndex = 4;
+			this.lblCols.Text = "Columnas:";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(858, 45);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(272, 17);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "Tovar Lizárraga Jesús Santiago || LISI 4-1";
+			this.label6.Click += new System.EventHandler(this.Label6Click);
+			// 
+			// lblName
+			// 
+			this.lblName.AutoSize = true;
+			this.lblName.Location = new System.Drawing.Point(516, 45);
+			this.lblName.Name = "lblName";
+			this.lblName.Size = new System.Drawing.Size(0, 17);
+			this.lblName.TabIndex = 6;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.ClientSize = new System.Drawing.Size(1300, 752);
+			this.Controls.Add(this.lblName);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.lblCols);
+			this.Controls.Add(this.lblRows);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.mapa);
+			this.Controls.Add(this.btnOpen);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "MainForm";
+			this.Text = "Sistema Final - Tovar Lizárraga";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.mapa)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label lblName;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtResumen;
+		private System.Windows.Forms.Label lblCols;
+		private System.Windows.Forms.Label lblRows;
+		private System.Windows.Forms.TextBox lblArray;
+		private System.Windows.Forms.TextBox txtX;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtY;
+		private System.Windows.Forms.Label X;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox txtRadio;
+		private System.Windows.Forms.Button btnCalcular;
+		private System.Windows.Forms.Label lblResultado;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox mapa;
+		private System.Windows.Forms.Button btnOpen;
+		
+		void LblArrayClick(object sender, System.EventArgs e)
+		{
+			
+		}
+	}
+}
---
+++ openRST/MainForm.cs
@@ -0,0 +1,513 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Santiago
+ * Date: 13/05/2024
+ * Time: 02:06 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace openRST
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		//Arreglo para conteo de la cantidad de celdas que son de tal categoría o leyenda	
+		private int[] categorias;
+		
+		//Arreglo de string para almacenar los nombres de las leyendas 
+		private string[] leyendas;
+		
+		
+		private int numeroLeyendas;
+		private int valorMax;
+		
+		
+		private int columns = 0;
+		private int rows = 0;
+		private string type = "";
+		private int[,] matriz;
+		
+		private Color[] colores = new Color[]{
+				Color.Black,
+				Color.Red,
+				Color.Green,
+				Color.Orange,
+				Color.Yellow,
+				Color.Turquoise,
+				Color.Blue,
+				Color.Beige,
+				Color.Crimson,
+				Color.DarkGray,
+				Color.DarkOrange,
+				Color.Olive,
+				Color.NavajoWhite,
+				Color.MediumSeaGreen,
+				Color.LightSkyBlue,
+				Color.LemonChiffon,
+				Color.Goldenrod,
+				Color.DarkViolet,
+				Color.DeepPink,
+				Color.Fuchsia,
+				Color.AliceBlue,
+			    Color.AntiqueWhite,
+			    Color.Aqua,
+			    Color.Aquamarine,
+			    Color.Azure,
+			    Color.Bisque,
+			    Color.BlanchedAlmond,
+			    Color.BlueViolet,
+			    Color.Brown,
+			    Color.BurlyWood,
+			    Color.CadetBlue,
+			    Color.Chartreuse,
+			    Color.Chocolate,
+			    Color.Coral,
+			    Color.CornflowerBlue,
+			    Color.Cornsilk,
+			    Color.Cyan,
+			    Color.DarkBlue,
+			    Color.DarkCyan,
+			    Color.DarkGoldenrod,
+			    Color.DarkGreen,
+			    Color.DarkKhaki,
+			    Color.DarkMagenta,
+			    Color.DarkOliveGreen,
+			    Color.DarkOrchid,
+			    Color.DarkRed,
+			    Color.DarkSalmon,
+			    Color.DarkSeaGreen,
+			    Color.DarkSlateBlue,
+			    Color.DarkSlateGray,
+			    Color.DarkTurquoise,
+			    Color.DeepSkyBlue,
+			    Color.DimGray,
+			    Color.DodgerBlue,
+			    Color.Firebrick,
+			    Color.FloralWhite,
+			    Color.ForestGreen,
+			    Color.Gainsboro,
+			    Color.GhostWhite,
+			    Color.Gold,
+			    Color.Gray,
+			    Color.GreenYellow,
+			    Color.Honeydew,
+			    Color.HotPink,
+			    Color.IndianRed,
+			    Color.Indigo,
+			    Color.Ivory,
+			    Color.Khaki,
+			    Color.Lavender,
+			    Color.LavenderBlush,
+			    Color.LawnGreen,
+			    Color.LightBlue,
+			    Color.LightCoral,
+			    Color.LightCyan,
+			    Color.LightGoldenrodYellow,
+			    Color.LightGray,
+			    Color.LightGreen,
+			    Color.LightPink,
+			    Color.LightSalmon,
+			    Color.LightSeaGreen,
+			    Color.LightSlateGray,
+			    Color.LightSteelBlue,
+			    Color.LightYellow,
+			    Color.Lime,
+			    Color.LimeGreen,
+			    Color.Linen,
+			    Color.Magenta,
+			    Color.Maroon,
+			    Color.MediumAquamarine,
+			    Color.MediumBlue,
+			    Color.MediumOrchid,
+			    Color.MediumPurple,
+			    Color.MediumSlateBlue,
+			    Color.MediumSpringGreen,
+			    Color.MediumTurquoise,
+			    Color.MediumVioletRed,
+			    Color.MidnightBlue,
+			    Color.MintCream,
+			    Color.MistyRose,
+			    Color.Moccasin,
+			    Color.Navy,
+			    Color.OldLace,
+			    Color.OliveDrab,
+			    Color.OrangeRed,
+			    Color.Orchid,
+			    Color.PaleGoldenrod,
+			    Color.PaleGreen,
+			    Color.PaleTurquoise,
+			    Color.PaleVioletRed,
+			    Color.PapayaWhip,
+			    Color.PeachPuff,
+			    Color.Peru,
+			    Color.Pink,
+			    Color.Plum,
+			    Color.PowderBlue,
+			    Color.Purple,
+			    Color.RosyBrown,
+			    Color.RoyalBlue,
+			    Color.SaddleBrown,
+			    Color.Salmon,
+			    Color.SandyBrown,
+			    Color.SeaGreen,
+			    Color.SeaShell,
+			    Color.Sienna,
+			    Color.Silver,
+			    Color.SkyBlue,
+			    Color.SlateBlue,
+			    Color.SlateGray,
+			    Color.Snow,
+			    Color.SpringGreen,
+			    Color.SteelBlue,
+			    Color.Tan,
+			    Color.Teal,
+			    Color.Thistle,
+			    Color.Tomato,
+			    Color.Violet,
+			    Color.Wheat,
+			    Color.White,
+			    Color.WhiteSmoke,
+			    Color.YellowGreen
+		};
+		private TrackBar trackBarZoom = new TrackBar();
+		
+		//Función de zoom
+		private void trackBarZoom_Scroll(object sender, EventArgs e) {
+		    TrackBar trackBar = sender as TrackBar;
+		    float zoomFactor = trackBar.Value;
+		    drawRaster(this.matriz, zoomFactor);
+		}
+		
+		//Inicializar trackBar
+		private void initTrack(){
+	        trackBarZoom.Minimum = 1;
+	        trackBarZoom.Maximum = 5;
+	        trackBarZoom.Value = 1;
+	        trackBarZoom.TickFrequency = 1;
+	        trackBarZoom.Dock = DockStyle.Bottom;
+	        trackBarZoom.Scroll += new EventHandler(trackBarZoom_Scroll);
+	        this.Controls.Add(trackBarZoom);
+		}
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			initTrack();
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		
+		//Abrir mapa
+		void Button1Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Archivos rst (*.rst*)|*.rst*"; 
+            openFileDialog.FilterIndex = 1; 
+            openFileDialog.RestoreDirectory = true; 
+
+           
+            if (openFileDialog.ShowDialog() == DialogResult.OK){
+            	
+                try{
+                   
+                    byte[] contenidoArchivo = File.ReadAllBytes(openFileDialog.FileName);
+                    lblName.Text = Path.GetFileName(openFileDialog.FileName);
+                    string archivoRDC = Path.ChangeExtension(openFileDialog.FileName, ".RDC");
+                    
+                    if (File.Exists(archivoRDC)){
+                    	
+                        	string[] contenidoRDC = File.ReadAllLines(archivoRDC);
+                        	
+                        	//Tipo de archivo
+                        	string[] type = contenidoRDC[2].Split(':');
+                        	this.type = type[1].Trim();
+                        	
+                        	if(this.type != "real"){
+                        		
+                        		//Num Columnas
+	                        	string[] columns = contenidoRDC[4].Split(':');
+	                        	this.columns = int.Parse(columns[1].Trim());
+	                        	
+	                        	//Num de Filas
+	                        	string[] rows = contenidoRDC[5].Split(':');
+	                        	this.rows = int.Parse(rows[1].Trim());
+	                        	
+	                        	//Valor máximo dentro del mapa
+	                        	string[] maxValue = contenidoRDC[16].Split(':');
+	                        	this.valorMax = int.Parse(maxValue[1].Trim());
+	                        	
+	                        	//Numero de leyendas que hay
+	                        	string[] numLeg = contenidoRDC[23].Split(':');
+	                        	this.numeroLeyendas = int.Parse(numLeg[1].Trim());
+	                        	
+	                        	
+	                        	if(this.numeroLeyendas > 0){
+	                        		//Se inicializa el arreglo de los nombres
+	                        		this.leyendas = new string[this.numeroLeyendas+1];
+	                        		this.leyendas[0] = "Vacio";
+	                        		
+	                        		//A partir de la linea 24 del archivo RDC vienen los nombres de las leyendas
+	                        		int lineArchivo = 24;
+	                        		for(int i = 1; i<this.numeroLeyendas+1;i++){
+	                        			
+	                        			//Se extrae el nombre de la leyenda
+	                        			string[] leg = contenidoRDC[lineArchivo].Split(':');
+	                        			this.leyendas[i] = leg[1].Trim();
+	                        			
+	                        			lineArchivo++;
+	                        		}
+	                        	}else{
+	                        		//Si no hay leyendas, se usa el max value que viene en el RDC
+	                        		this.leyendas = new string[this.valorMax+1];
+	                        		this.numeroLeyendas = this.valorMax;
+	                        		for(int i = 0; i<this.valorMax+1;i++){
+	                        			
+	                        			this.leyendas[i] = i.ToString();
+	                        			
+	                        		}
+	                        	}
+	                        	
+	                        	lblCols.Text = "Columnas: "+this.columns.ToString();
+	                        	lblRows.Text = "Filas: "+this.rows.ToString();
+	                        	
+	                        	
+	                        	//Dibujado del mapa
+	                        	if(this.type == "byte"){
+	                        		
+	                        		float initialZoomFactor = 1.0f;
+	                        		this.matriz = binarytoDecimal(contenidoArchivo);
+	                        		drawRaster(this.matriz, initialZoomFactor);
+	                        		trackBarZoom.Value = 1;
+	                        		
+	                        	}else if (this.type == "integer"){
+	                        		
+	                        		this.matriz = binarytoDecimalInt(contenidoArchivo);
+	                        		float initialZoomFactor = 1.0f;
+	                        		drawRaster(this.matriz, initialZoomFactor);
+	                        		trackBarZoom.Value = 1;
+	                        		
+	                        	}
+	                        	//Sección de vecinos activada
+	                        	panel1.Enabled = true;
+                        	}else{
+                        		MessageBox.Show("Elija un archivo de tipo de integer o byte");
+                        	}
+                        	
+                     }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al leer el archivo: " + ex.Message);
+                }
+            }
+		}
+		
+		//Leer bytes y pasarlos a decimal
+		public int[,] binarytoDecimal(byte[] raster){
+			int [,] matrizDecimal = new int[this.rows, this.columns];
+			int aux = 0;
+			for(int i = 0;i< this.rows;i++){
+				for(int j = 0; j < this.columns; j++){
+					if(aux < raster.Length){
+						matrizDecimal[i,j] = int.Parse(raster[aux].ToString());
+						aux++;
+					}else{
+						matrizDecimal[i,j] = 0;
+					}
+				}
+			}
+			return matrizDecimal;
+		}
+		
+		//Leer integers y pasarlos a decimal
+		public int[,] binarytoDecimalInt(byte[] raster){
+			int [,] matrizDecimal = new int[this.rows, this.columns];
+			int aux = 0;
+			for(int i = 0;i< this.rows;i++){
+				for(int j = 0; j < this.columns; j++){
+					if (aux + 1 < raster.Length){
+						
+		                int valorDecimal = BitConverter.ToInt16(raster, aux);
+		                
+		                matrizDecimal[i, j] = valorDecimal;
+		                aux += 2;
+		            }else{
+						matrizDecimal[i,j] = 0;
+					}
+				}
+			}
+			return matrizDecimal;
+		}
+		
+		//Dibujar mapa
+		private void drawRaster(int[,] matrizDecimales, float zoomFactor) {
+		    
+			txtResumen.Text = "";
+		    lblArray.Text = "";
+		    lblResultado.Text = "Vecinos: ";
+		    int rows = this.rows;
+		    int columns = this.columns;
+		
+		    //Crear Bitmap original
+		    Bitmap originalBitmap = new Bitmap(columns, rows);
+		    for (int i = 0; i < rows; i++) {
+		        for (int j = 0; j < columns; j++) {
+		            Color pixel = this.colores[matrizDecimales[i, j]];
+		            originalBitmap.SetPixel(j, i, pixel);
+		        }
+		    }
+		
+		    //Calcular el tamaño del Bitmap con zoom
+		    int zoomedWidth = (int)(columns * zoomFactor);
+		    int zoomedHeight = (int)(rows * zoomFactor);
+		
+		    //Crear el Bitmap con zoom
+		    Bitmap zoomedBitmap = new Bitmap(zoomedWidth, zoomedHeight);
+		    using (Graphics g = Graphics.FromImage(zoomedBitmap)) {
+		    	
+		        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+		        g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
+		        g.DrawImage(originalBitmap, new Rectangle(0, 0, zoomedWidth, zoomedHeight));
+		        
+		    }
+		
+		    //Tamaño del pictureBox
+		    mapa.Size = new Size(zoomedWidth, zoomedHeight);
+		    mapa.Image = zoomedBitmap;
+		    this.mapa.MouseClick -= new MouseEventHandler(mapa_MouseClick);
+			this.mapa.MouseClick += new MouseEventHandler(mapa_MouseClick);
+
+		}
+
+		//Función que calcula los vecinos
+		void calcularVecinos(int[,] matriz, int radio, int posX, int posY){
+		    txtResumen.Text = "";
+		    lblArray.Text = "";
+		
+		    this.categorias = new int[this.valorMax + 1];
+		
+		    if (posY >= this.rows || posX >= this.columns)
+		    {
+		        MessageBox.Show("Celda fuera de rango", "Error", MessageBoxButtons.OK);
+		        return;
+		    }
+		
+		    StringBuilder vecinos = new StringBuilder();
+		    StringBuilder res = new StringBuilder();
+		    int limIzq = Math.Max(posX - radio, 0);
+		    int limDer = Math.Min(posX + radio, this.columns - 1);
+		    int limUp = Math.Max(posY - radio, 0);
+		    int limDown = Math.Min(posY + radio, this.rows - 1);
+		    int contador = 0;
+		
+		    //Recorrer la submatriz
+		    for (int i = limUp; i <= limDown; i++)
+		    {
+		        for (int j = limIzq; j <= limDer; j++)
+		        {
+		            contador++;
+		            if (!(i == posY && j == posX))
+		            {
+		                //Almacenar vecinos
+		                vecinos.AppendFormat("[{0},{1}], ", j, i);
+		
+		                //Conteo de categorías
+		                this.categorias[matriz[i, j]]++;
+		            }
+		        }
+		        vecinos.AppendLine();
+		    }
+		
+		    //Realizar la lista de vecinos y el resumen
+		    lblArray.Text = vecinos.ToString();
+		    lblResultado.Text = "Vecinos: " + (contador - 1).ToString();
+		    txtResumen.Text = "Los vecinos alrededor del punto son:" + Environment.NewLine;
+		
+		    for (int it = 0; it <= this.numeroLeyendas; it++)
+		    {
+		        if (this.categorias[it] > 0)
+		        {
+		        	res.AppendFormat("{0} pertenecientes a {1}\n", this.categorias[it].ToString(), this.leyendas[it]);
+		        	res.AppendLine();
+		        }
+		    }
+		    txtResumen.Text = res.ToString();
+		}
+
+		
+		
+		//Cálculo de vecinos
+		void BtnCalcularClick(object sender, EventArgs e){
+			
+	    	if (!string.IsNullOrEmpty(txtX.Text) && !string.IsNullOrEmpty(txtY.Text) && !string.IsNullOrEmpty(txtRadio.Text)){
+		        
+				try{
+					
+		            int x = int.Parse(txtX.Text);
+		            int y = int.Parse(txtY.Text);
+		            int radio = int.Parse(txtRadio.Text);
+		            
+		            calcularVecinos(this.matriz, radio, x, y);
+		        }
+		        catch (FormatException){
+					
+		            MessageBox.Show("Todos los valores tienen que ser números enteros.");
+		        }
+		    }else{
+				
+		        MessageBox.Show("Llene todos los campos antes de calcular.");
+		    }
+		}
+		
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		//Hacer clic dentro del mapa para obtener coordenadas
+		private void mapa_MouseClick(object sender, MouseEventArgs e){
+			
+		    //Coordenadas del clic en el mapa
+		    int mouseX = e.X;
+		    int mouseY = e.Y;
+		
+		    //Obtener el factor de zoom actual desde el TrackBar
+		    float zoomFactor = trackBarZoom.Value;
+		
+		    //Convertir las coordenadas de clic al sistema de coordenadas original del mapa
+		    int originalX = (int)(mouseX / zoomFactor);
+		    int originalY = (int)(mouseY / zoomFactor);
+		
+		    //Validar que las coordenadas están dentro del rango válido
+		    if (originalX >= 0 && originalX < columns && originalY >= 0 && originalY < rows){
+		        txtX.Text = originalX.ToString();
+		        txtY.Text = originalY.ToString();
+		    }
+		}
+		
+		void Label6Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
